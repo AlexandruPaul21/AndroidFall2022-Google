5454b8c012e808b2f6197930e10bc1bb1272dc56
alexandrusirbu <alexandru.sirbu2104@gmail.com>
2023-01-07 13:30:05 +0200

    Card view

--- app/src/main/java/com/alexsirbu/androidfall2022/storage/StorageActivity.java
+++ app/src/main/java/com/alexsirbu/androidfall2022/storage/StorageActivity.java
@@ -1,11 +1,16 @@
 package com.alexsirbu.androidfall2022.storage;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.alexsirbu.androidfall2022.R;
+
+import java.util.List;
 
 public class StorageActivity extends AppCompatActivity {
 
@@ -14,12 +19,34 @@ public class StorageActivity extends AppCompatActivity {
     protected static final String COLOR = "color";
     protected static final String SLIDES = "slides";
 
+    private WordViewModel wordViewModel;
+
+    private Button buttonInsertWord;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
 
         useSharedPreferences();
+
+        buttonInsertWord = findViewById(R.id.buttonInsertWord);
+
+        wordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+        wordViewModel.getWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                Log.e(TAG, words.toString());
+            }
+        });
+
+        insertNewWords();
+    }
+
+    private void insertNewWords() {
+        buttonInsertWord.setOnClickListener(view -> {
+            wordViewModel.insert(new Word("android " + System.currentTimeMillis()));
+        });
     }
 
     private void useSharedPreferences() {
@@ -29,13 +56,14 @@ public class StorageActivity extends AppCompatActivity {
 
         //get values
         String valueColor = ApplicationData.getStringValueFromSharedPreferences(StorageActivity.this, COLOR);
-        Toast.makeText(StorageActivity.this, valueColor, Toast.LENGTH_LONG).show();
+        //Toast.makeText(StorageActivity.this, valueColor, Toast.LENGTH_LONG).show();
 
         int valuesSlides = ApplicationData.getIntValueFromSharedPreferences(StorageActivity.this, SLIDES);
-        Toast.makeText(StorageActivity.this, valuesSlides + "", Toast.LENGTH_LONG).show();
+        //Toast.makeText(StorageActivity.this, valuesSlides + "", Toast.LENGTH_LONG).show();
 
         //delete
         ApplicationData.deleteAllValuesFromSharedPreferences(StorageActivity.this);
+        Log.wtf(TAG, "It is gone :'(");
 
     }
 }
